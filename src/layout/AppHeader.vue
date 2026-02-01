8a30c6b633b8ca6bebff0b3cc3ae4514cf28ed70
Katarina Floyd <katarina.floyd1@gmail.com>
2026-02-01 14:29:07 +0100

    feat: add app header component

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1 class="logo">Eurowings</h1>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+  padding: 1rem;
+  background: #fff;
+  border-bottom: 1px solid #eee;
+}
+
+.logo {
+  font-size: 1.25rem;
+  font-weight: 600;
+}
+</style>
